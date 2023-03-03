5aa2946745f0edddb2a125db664222804752eed1
michael <mchege78@gmail.com>
2023-03-03 16:31:37 +0300

    added header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,30 @@
 import React from "react";
+import michael from "../../assets/Michael.JPG";
 import "./header.css";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="header section__padding" id="header">
+      <div className="header__image">
+        <img src={michael} alt="Michael Kuria" />
+      </div>
+      <div className="header__content">
+        <p>
+          Hi, my name is{" "}
+          <span className="gradient__background">Michael Kuria</span>
+        </p>
+        <p>I'm a Full Stack Developer.</p>
+      </div>
+      <div className="header__btn">
+        <a
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="gradient__background"
+        >
+          Download Resume
+        </a>
+      </div>
+    </header>
+  );
 }
